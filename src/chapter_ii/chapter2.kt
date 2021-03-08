f23707e092c5334a1ed8b182bc59e9bfe91642a5
Alex Allafi <alex.allafi@skroutz.gr>
2021-03-08 20:44:43 +0200

    Create a formatAmount fun and include it where needed

--- src/chapter_ii/chapter2.kt
+++ src/chapter_ii/chapter2.kt
@@ -1,5 +1,7 @@
 package chapter_ii
 
+
+
 class Account(private val debits: Float,
               private val credits: Float,
               private val fees: Float,
@@ -7,30 +9,22 @@ class Account(private val debits: Float,
 
     fun printBalance() {
 
-        println("Debits: %10.2f".format(debits))
-        println("Credits: %10.2f".format(credits))
+        println()
 
-        if (fees < 0) {
-            println("Fees:    -%.2f".format(-fees))
-        } else {
-            println("Fees:    %10.2f".format(fees))
-        }
+        println("Debits:  ${formatAmount(debits)}")
+        println("Credits: ${formatAmount(credits)}")
+        println("Fees:    ${formatAmount(fees)}")
         println("    ----")
-        if (balance < 0) {
-            println("Balance: -%.2f".format(-balance))
-        } else {
-            println("Balance: %10.2f".format(balance))
-        }
+        println("Balance: ${formatAmount(balance)}")
     }
+
+    private fun formatAmount(f: Float): String = "%-10.2f".format(f)
 }
 
 fun main() {
-    val a = Account(1355F, 1000F, 45F, 2500F)
-    val a1 = Account(1355F, 1000F, -45F, -200F)
-    val a2 = Account(1355F, 1000F, -45F, 2500F)
 
-    a.printBalance()
-    a1.printBalance()
-    a2.printBalance()
+    Account(1355F, 1000F, 45F, 2500F).also { it.printBalance() }
+    Account(1355F, 1000F, -45F, -200F).also { it.printBalance() }
+    Account(1355F, 1000F, -45F, 2500F).also { it.printBalance() }
 }
 
